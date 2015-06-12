60490608e0446c39e533e356765ac5c3fdbbe471
Slee Woo <mail@sleewoo.com>
2015-06-12 11:13:44 +0300

    overriding Minitest::Mock.respond_to? method to add respond_to? message @actual_calls

--- test/setup.rb
+++ test/setup.rb
@@ -2,6 +2,18 @@ require 'json'
 require 'minitest/autorun'
 require 'clover'
 
+module Minitest
+  class Mock
+    def respond_to?(sym, include_private = false)
+      retval = __respond_to?(sym, include_private)
+      if @expected_calls.key?(:respond_to?)
+        @actual_calls[:respond_to?] << {retval: retval, args: [sym, include_private]}
+      end
+      retval
+    end
+  end
+end
+
 def proxy obj
   Clover::Assert.new(obj)
 end
