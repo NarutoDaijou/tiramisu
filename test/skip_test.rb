2fd1e61ae5a507df2154adbbd1fe9181a060d170
Slee Woo <mail@sleewoo.com>
2015-06-12 11:23:01 +0300

    testing skip from inside test

--- test/skip_test.rb
+++ test/skip_test.rb
@@ -16,4 +16,25 @@ describe :skip do
     assert_equal 1, data['skips']
     assert_equal 1, data['tests']
   end
+
+  it 'should skip inside test' do
+    r, w = IO.pipe
+    fork do
+      r.close
+      buffer = []
+      Clover.define_and_register_a_spec :skip_test, proc {
+        test :x do
+          buffer << 'a'
+          skip
+          buffer << 'b'
+        end
+      }
+      Clover.run
+      w.print({skips: Clover.skips.size, buffer: buffer}.to_json)
+    end
+    w.close
+    data = JSON.parse(r.read)
+    assert_equal 1, data['skips']
+    assert_equal ["a"], data['buffer']
+  end
 end
