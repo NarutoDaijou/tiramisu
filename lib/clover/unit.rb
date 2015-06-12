14c0f7679ca62a73ca984ea4a265adca6fc4d989
Slee Woo <mail@sleewoo.com>
2015-06-12 10:04:50 +0300

    define tests

--- lib/clover/unit.rb
+++ lib/clover/unit.rb
@@ -17,5 +17,20 @@ module Clover
       return unless block
       Clover.define_and_register_a_context(label, block, self)
     end
+
+    # define a test
+    #
+    # @param label
+    # @param &block
+    def test label, &block
+      return unless block
+      tests[label] = Clover.identity_string(:test, label, block)
+      define_method(tests[label], &block)
+    end
+    alias it test
+
+    def tests
+      @__tiramisu_tests__ ||= {}
+    end
   end
 end
