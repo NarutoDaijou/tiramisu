5278c4d535cccb7e8d71c719bf63410583036601
Slee Woo <mail@sleewoo.com>
2015-06-12 10:06:21 +0300

    define hooks

--- lib/clover/unit.rb
+++ lib/clover/unit.rb
@@ -32,5 +32,80 @@ module Clover
     def tests
       @__tiramisu_tests__ ||= {}
     end
+
+    # run some code before/around/after any or specific tests
+    #
+    # @example call :define_users before any test (in current spec)
+    #   spec User do
+    #     before do
+    #       @users = ...
+    #     end
+    #
+    #     test :login do
+    #       # @users available here
+    #     end
+    #   end
+    #
+    # @example run only after :photos test
+    #   spec User do
+    #     after :photos do
+    #       ...
+    #     end
+    #
+    #     test :photos do
+    #       ...
+    #     end
+    #   end
+    #
+    # @example run around :authorize and :authenticate tests
+    #   spec User do
+    #     around :authorize, :authenticate do |&test|
+    #       SomeApi.with_auth do
+    #         test.call # run the test
+    #       end
+    #     end
+    #
+    #     test :authorize do
+    #       # will run inside `with_auth` block
+    #     end
+    #
+    #     test :authenticate do
+    #       # same
+    #     end
+    #   end
+    #
+    # @note if multiple hooks defined for same test only the last one will run
+    # @note named hooks will have precedence over wildcard ones
+    #
+    # @example  named hooks have precedence over wildcard ones
+    #   before :math do
+    #     # named hook, to run only before :math test
+    #   end
+    #
+    #   before do
+    #     # wildcard hook, to run before any test
+    #   end
+    #
+    #   test :math do
+    #     # only `bofore :math` hook will run here
+    #   end
+    #
+    [
+      :before,
+      :around,
+      :after
+    ].each do |hook|
+      define_method hook do |*tests,&block|
+        block || raise(ArgumentError, 'block missing')
+        meth = :"__tiramisu_hooks_#{hook}_#{block.source_location.join}__"
+        tests = [:__tiramisu_hooks_any__] if tests.empty?
+        tests.each {|t| (hooks[hook] ||= {})[t] = meth}
+        define_method(meth, &block)
+      end
+    end
+
+    def hooks
+      @__tiramisu_hooks__ ||= Tiramisu.void_hooks
+    end
   end
 end
