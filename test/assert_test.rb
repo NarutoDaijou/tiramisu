822f4fa268a0de832ada22e6a8239d980c88918a
Slee Woo <mail@sleewoo.com>
2015-06-12 11:08:14 +0300

    testing === and != assertions

--- test/assert_test.rb
+++ test/assert_test.rb
@@ -5,4 +5,18 @@ describe :assert do
     x == :x
     x.verify
   end
+
+
+  it 'pass ===' do
+    x = mock(:===, :x)
+    x === :x
+    x.verify
+  end
+
+  it 'pass !=' do
+    x = mock(:!=, :x)
+    x != :x
+    x.verify
+  end
+
 end
