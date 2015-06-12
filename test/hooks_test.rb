0304f784dad77bb12a73982650a3dcc4a87f86d0
Slee Woo <mail@sleewoo.com>
2015-06-12 11:16:45 +0300

    testing named hooks

--- test/hooks_test.rb
+++ test/hooks_test.rb
@@ -10,4 +10,16 @@ describe :hooks do
     end
     assert_equal 2, called
   end
+
+  it 'calls named hooks' do
+    called = 0
+    spec rand do
+      after(:b) {called += 1}
+      test(:a) {}
+      test(:b) {}
+      run(:a)
+      run(:b)
+    end
+    assert_equal 1, called
+  end
 end
