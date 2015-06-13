3011bb2daff4b4a2bd7e01591c4ad85a06996c85
Slee Woo <mail@sleewoo.com>
2015-06-13 13:02:04 +0300

    refactoring `raises` assertion

--- lib/clover/assertions/raises.rb
+++ lib/clover/assertions/raises.rb
@@ -1,39 +1,35 @@
 assert :raises do |proc, type, message, &block|
+  Clover.raised_as_expected?(proc, type, message, block)
+  true # if arriving here without failing the assertion is passed
+end
 
-  e = nil
-  begin
-    proc.call
-  rescue Exception => e
-  end
-  source_location = Clover.relative_source_location(proc)
-
-  e.is_a?(Exception) ||
-    fail('Expected a exception to be raised at %s:%s' % source_location)
-
-  if block
-    block.call(exception) ||
-      fail('Looks like the block at %s:%s did not raise as expected' % source_location)
-  end
-
-  if type
-    type == type ||
-      fail('Expected a %s to be raised at %s:%s' % [
-          type,
-          *source_location
-        ],
-        'Instead a %s raised' % type
-      )
+module Clover
+  def raised_as_expected? proc, expected_type, expected_message, block
+    e = nil
+    begin
+      proc.call
+    rescue Exception => e
+    end
+    source_location = Clover.relative_source_location(proc)
+    raised?(e, source_location)
+    return raised_as_expected_by_block?(e, block, source_location) if block
+    if expected_type
+      raised_expected_type?(e.class, expected_type, source_location)
+    end
+    if expected_message
+      raised_expected_message?(e.message, expected_message, source_location)
+    end
   end
 
-  if message
-    regexp = message.is_a?(Regexp) ? message : /\A#{message}\z/
-    message =~ regexp ||
-      fail(
-        'Expected the exception raised at %s:%s' % source_location,
-        'to match "%s"' % regexp.source,
-        'Instead it looks like',
-        message ? message : message.inspect
-      )
+  def raised? x, source_location
   end
-  true
+
+  def raised_expected_type? type, expected_type, source_location
+  end
+
+  def raised_expected_message? message, expected_message, source_location
+  end
+
+  def raised_as_expected_by_block? exception, block, source_location
+  end
 end
