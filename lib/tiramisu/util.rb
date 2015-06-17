fd8d8f08a5e859bf98469142f5158b542198bfb5
Slee Woo <mail@sleewoo.com>
2015-06-17 12:10:23 +0300

    call given block and catch thrown symbol, if any

--- lib/tiramisu/util.rb
+++ lib/tiramisu/util.rb
@@ -58,4 +58,39 @@ module Tiramisu
   def pwd *args
     File.join(Dir.pwd, *args.map!(&:to_s))
   end
+
+  # call given block and catch thrown symbol, if any
+  #
+  # @param [Proc] block
+  # @param [Symbol] expected_symbol
+  #
+  def catch_symbol block, expected_symbol
+    thrown_symbol, thrown_value = nil
+    begin
+      if expected_symbol
+        thrown_value = catch :__tiramisu__nothing_thrown do
+          catch expected_symbol do
+            block.call
+            throw :__tiramisu__nothing_thrown, :__tiramisu__nothing_thrown
+          end
+        end
+        thrown_symbol = expected_symbol unless thrown_value == :__tiramisu__nothing_thrown
+      else
+        block.call
+      end
+    rescue => e
+      raise(e) unless thrown_symbol = extract_thrown_symbol(e)
+    end
+    [thrown_symbol, thrown_value]
+  end
+
+  # extract thrown symbol from given exception
+  #
+  # @param exception
+  #
+  def extract_thrown_symbol exception
+    return unless exception.is_a?(Exception)
+    return unless s = exception.message.scan(/uncaught throw\W+(\w+)/).flatten[0]
+    s.to_sym
+  end
 end
