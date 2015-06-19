297e25e807a424b976b208f59460d117c5f3e33c
Slee Woo <mail@sleewoo.com>
2015-06-19 14:13:18 +0300

    preparing expectation tests

---
+++ test/receive_test.rb
@@ -0,0 +1,14 @@
+describe :receive do
+
+  it 'should pass when a message expected and received' do
+  end
+
+  it 'should fail when a expected message not received' do
+  end
+
+  it 'should pass when no message expected and no message received' do
+  end
+
+  it 'should fail when unexpected message received' do
+  end
+end
