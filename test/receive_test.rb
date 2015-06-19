d75a499eb8a917833566ddfe0610af5349d0f018
Slee Woo <mail@sleewoo.com>
2015-06-19 14:22:20 +0300

    it 'should pass when a message expected and received' do

--- test/receive_test.rb
+++ test/receive_test.rb
@@ -1,6 +1,14 @@
 describe :receive do
 
   it 'should pass when a message expected and received' do
+    this = self
+    spec rand do
+      test :test do
+        mock = expect(:x).to_receive(:class)
+        mock.class
+      end
+      this.assert_equal :__tiramisu_passed__, run(:test)
+    end
   end
 
   it 'should fail when a expected message not received' do
