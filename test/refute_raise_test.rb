20621420a43c2afe70de38ded667d341842bcb60
Slee Woo <mail@sleewoo.com>
2015-06-20 15:01:42 +0300

    it 'should fail if exception of specific type negated but nothing raised'

--- test/refute_raise_test.rb
+++ test/refute_raise_test.rb
@@ -21,6 +21,12 @@ describe :raises do
     assert r.reason.any? {|l| l =~ /unexpected NameError raised/}
   end
 
+  it 'should fail if exception of specific type negated but nothing raised' do
+    r = raisetest(NameError) {}
+    assert r.class == Tiramisu::Failures::Generic
+    assert r.reason.any? {|l| l =~ /Expected a exception to be raised/}
+  end
+
   it 'should pass if it raises a exception type different from negated one' do
     r = raisetest(ArgumentError) {x}
     assert_equal true, r
