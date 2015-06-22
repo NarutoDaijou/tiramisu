bc5b806f25df9282cf634a6c169114e7c8d62956
Slee Woo <mail@sleewoo.com>
2015-06-22 13:59:55 +0300

    refactoring `raise` assertion tests

--- test/raise_test.rb
+++ test/raise_test.rb
@@ -1,5 +1,5 @@
-describe :raises do
-  def raisetest type = nil, message = nil, &block
+describe :raise do
+  def assert_raise type = nil, message = nil, &block
     catch :__tiramisu_status__ do
       assert = Tiramisu::Assert.new(nil, :assert, block)
       if type.is_a?(Proc)
@@ -11,52 +11,52 @@ describe :raises do
   end
 
   it 'should pass if any exception raised' do
-    r = raisetest {x}
+    r = assert_raise {x}
     assert_equal true, r
   end
 
   it 'should fail if nothing raised' do
-    r = raisetest {}
+    r = assert_raise {}
     assert r.class == Tiramisu::Failures::Generic
     assert r.reason.any? {|l| l =~ /Expected a exception to be raised/}
   end
 
   it 'should pass if type matching' do
-    r = raisetest(NameError) {x}
+    r = assert_raise(NameError) {x}
     assert_equal true, r
   end
 
   it 'should fail if wrong exception raised' do
-    r = raisetest(ArgumentError) {x}
+    r = assert_raise(ArgumentError) {x}
     assert r.class == Tiramisu::Failures::Generic
     assert r.reason.any? {|l| l =~ /Expected a ArgumentError to be raised/}
   end
 
   it 'should pass if both type and message matches' do
-    r = raisetest(NameError, /undefined local variable or method/) {x}
+    r = assert_raise(NameError, /undefined local variable or method/) {x}
     assert_equal true, r
   end
 
   it 'should fail if message does not match' do
-    r = raisetest(NameError, /blah/) {x}
+    r = assert_raise(NameError, /blah/) {x}
     assert r.class == Tiramisu::Failures::Generic
     assert r.reason.any? {|l| l =~ /to match/}
   end
 
   it 'should fail if nothing raised and validation block given' do
-    r = raisetest(proc {}) {}
+    r = assert_raise(proc {}) {}
     assert r.class == Tiramisu::Failures::Generic
     assert r.reason.any? {|l| l =~ /Expected a exception to be raised/}
   end
 
   it 'should pass if given block validates exception' do
-    r = raisetest(proc {|e| e.class == NameError}) {x}
+    r = assert_raise(proc {|e| e.class == NameError}) {x}
     assert_equal true, r
   end
 
   it 'should fail if  given block does not validate exception' do
     p = proc {|e| e.class == ArgumentError}
-    r = raisetest(p) {x}
+    r = assert_raise(p) {x}
     assert r.class == Tiramisu::Failures::Generic
     assert r.reason.any? {|l| l =~ /did not raise as expected/}
   end
