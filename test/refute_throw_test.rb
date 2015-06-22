04fb82eccf8478cf12877ddb880ceaa4a7410326
Slee Woo <mail@sleewoo.com>
2015-06-22 13:09:28 +0300

    it 'should pass when thrown symbol does not match negated one'

--- test/refute_throw_test.rb
+++ test/refute_throw_test.rb
@@ -15,6 +15,11 @@ describe :refute_throw do
     assert r.reason.any? {|l| l =~ /Not expected a symbol to be thrown/}
   end
 
+  it 'should pass when thrown symbol does not match negated one' do
+    r = refute_throw(:x) {throw :y}
+    assert_equal true, r
+  end
+
   it 'should fail when a negated symbol expected and nothing thrown' do
     r = refute_throw(:x) {}
     assert r.reason.any? {|l| l =~ /Expected a symbol to be thrown/}
