2f06f63dccf052ac732bc79089cb5dfedac1d892
Slee Woo <mail@sleewoo.com>
2015-06-25 11:02:49 +0300

    implementing `and_return` validator

--- lib/tiramisu/mock/return.rb
+++ lib/tiramisu/mock/return.rb
@@ -14,15 +14,29 @@ module Tiramisu
       elsif @expected_messages.size > 1
         values.size == @expected_messages.size ||
           Kernel.raise(ArgumentError, "Wrong number of arguments, #{values.size} for #{@expected_messages.size}")
+        values.all? {|x| x.is_a?(Array)} ||
+          Kernel.raise(ArgumentError, 'Please provide expected return values as arrays, one array per expected message')
         values
       else
-        values
+        [values]
       end
       self
     end
 
     private
     def __assert_message_returned_correct_value__ msg, i
+      return unless @return
+      if @return.is_a?(Proc)
+        __received_messages__[msg].find {|log| @return.call(msg, log[:returned])} || Tiramisu.fail([
+          'Looks like :%s message never returned expected value' % msg,
+          'See validation block'
+        ], @caller)
+      else
+        __received_messages__[msg].find {|log| log[:returned] == @return[i]} || Tiramisu.fail([
+          'Looks like :%s message never returned expected value:' % msg,
+          Tiramisu.pp(@return[i])
+        ], @caller)
+      end
     end
   end
 end
