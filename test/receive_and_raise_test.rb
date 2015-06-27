a2026f14db543ca6d73dc92a77be520db3e77f20
Slee Woo <mail@sleewoo.com>
2015-06-27 15:02:22 +0300

    it 'should pass when received message raises whatever error'

--- test/receive_and_raise_test.rb
+++ test/receive_and_raise_test.rb
@@ -11,6 +11,17 @@ describe :receive_and_raise do
     end
   end
 
+  it 'should pass when received message raises whatever error' do
+    this = self
+    spec rand do
+      test :test do
+        x = expect(:x).to_receive(:y).and_raise
+        x.y
+      end
+      this.assert_equal :__tiramisu_passed__, run(:test)
+    end
+  end
+
   it 'should fail when received message does not raise' do
     this = self
     spec rand do
@@ -18,7 +29,7 @@ describe :receive_and_raise do
         x = expect(:x).to_receive(:to_s).and_raise
         x.to_s
       end
-      this.assert run(:test).reason.any? {|l| l =~ /Expected a exception to be raised/}
+      this.assert_match /Expected a exception to be raised/, run(:test).reason[0]
     end
   end
 
