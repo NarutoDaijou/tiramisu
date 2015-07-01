f9daa6ab24c0a10681d8cbf0a4f9c1a50e770265
Slee Woo <mail@sleewoo.com>
2015-07-01 14:19:59 +0300

    it 'should pass when received message throws expected symbol'

--- test/receive_and_throw_test.rb
+++ test/receive_and_throw_test.rb
@@ -10,4 +10,15 @@ describe :receive_and_throw do
       this.assert_equal :__tiramisu_passed__, run(:test)
     end
   end
+
+  it 'should pass when received message throws expected symbol' do
+    this = self
+    spec rand do
+      test :test do
+        x = expect(Class.new {define_singleton_method(:y) {throw :z}}).to_receive(:y).and_throw(:z)
+        x.y
+      end
+      this.assert_equal :__tiramisu_passed__, run(:test)
+    end
+  end
 end
