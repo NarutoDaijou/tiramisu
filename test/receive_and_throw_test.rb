54cdd3dd968587315690e15f871dfc9133def803
Slee Woo <mail@sleewoo.com>
2015-07-01 14:26:16 +0300

    it 'should fail when received message throws correct symbol but wrong value'

--- test/receive_and_throw_test.rb
+++ test/receive_and_throw_test.rb
@@ -43,4 +43,26 @@ describe :receive_and_throw do
       this.assert_match /Expected :a to be thrown/, run(:test).reason[0]
     end
   end
+
+  it 'should pass when received message throws expected symbol and value' do
+    this = self
+    spec rand do
+      test :test do
+        x = expect(Class.new {define_singleton_method(:y) {throw :a, :b}}).to_receive(:y).and_throw(:a, :b)
+        x.y
+      end
+      this.assert_equal :__tiramisu_passed__, run(:test)
+    end
+  end
+
+  it 'should fail when received message throws correct symbol but wrong value' do
+    this = self
+    spec rand do
+      test :test do
+        x = expect(Class.new {define_singleton_method(:y) {throw :a, :b}}).to_receive(:y).and_throw(:a, :z)
+        x.y
+      end
+      this.assert_match /Wrong value thrown/, run(:test).reason[0]
+    end
+  end
 end
